10359c5da94ef348ccecbd7775c0a27b2f20c9a9
Roylouis-web <powellleroy488@gmail.com>
2023-09-03 17:15:26 +0000

    All done

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ import * as bodyParser from 'body-parser';
 import router from './routes/index';
 
 const app = express();
+const port = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/', router);
-
-app.listen(process.env.PORT || 5000, () => console.log('Server running on port 5000'));
+app.listen(port, () => console.log(`Server running on port ${port}`));
 
 export default app;
