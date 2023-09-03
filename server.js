12a25872d64507a558843a3d8ccc4b8c9e4b24d5
Roylouis-web <powellleroy488@gmail.com>
2023-09-03 17:27:18 +0000

    All done

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import * as bodyParser from 'body-parser';
 import router from './routes/index';
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = parseInt(process.env.PORT) || 5000;
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/', router);
